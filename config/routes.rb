68281658a3ced100ec7d49e543e79ff66f29f9bc
sevenjra <sevenjra@gmail.com>
2022-06-05 15:03:46 -0300

    commit inicial

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  devise_for :profiles
+  devise_for :admins
+  get 'welcome/index'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'welcome#index'
+end
